d2a66d82583f9a7d3f14ff52bbfd1b04819a23c3
YamagishiHiroki <ymgshrk.126@gmail.com>
2022-01-18 16:17:14 +0900

    テストが通るように修正 #5

--- src/spec/requests/users_spec.rb
+++ src/spec/requests/users_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
     it "リクエストが成功すること" do
-      get users_path
-      # get '/'
+      get '/'
       expect(response).to have_http_status(200)
     end
   end
