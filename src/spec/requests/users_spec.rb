504375c0db2fcccbbd879c4ca0086146a0f59505
YamagishiHiroki <ymgshrk.126@gmail.com>
2022-01-18 10:12:19 +0900

    Usersコントローラーのテストを追加 #5

---
+++ src/spec/requests/users_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  describe "GET /users" do
+    it "リクエストが成功すること" do
+      # get users_path
+      get '/'
+      expect(response).to have_http_status(200)
+    end
+  end
+end
