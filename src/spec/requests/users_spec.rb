312906e0d41877c171f002a52f631cc54ce968f3
YamagishiHiroki <ymgshrk.126@gmail.com>
2022-01-18 22:34:05 +0900

    RuboCopの導入 #9

--- src/spec/requests/users_spec.rb
+++ src/spec/requests/users_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "リクエストが成功すること" do
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users' do
+    it 'リクエストが成功すること' do
       get '/'
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
   end
 end
