53adbbbd76240fa45d31544333ed9b13e5f9feb3
YamagishiHiroki <ymgshrk.126@gmail.com>
2022-01-20 09:47:45 +0900

    StaticPagesコントローラーのテストを追加 #11

--- src/spec/requests/static_pages_spec.rb
+++ src/spec/requests/static_pages_spec.rb
@@ -2,15 +2,15 @@ require "rails_helper"
 
 RSpec.describe "StaticPages", type: :request do
   describe "GET /home" do
-    it "returns http success" do
-      get "/static_pages/home"
+    it "リクエストが成功すること" do
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /contact" do
-    it "returns http success" do
-      get "/static_pages/contact"
+    it "リクエストが成功すること" do
+      get contact_path
       expect(response).to have_http_status(:success)
     end
   end
