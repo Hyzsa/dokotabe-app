a10ec09f75d2de038c01343b294fc5a2c0147aa5
YamagishiHiroki <ymgshrk.126@gmail.com>
2022-01-21 22:31:20 +0900

    Rubocopの指摘に対応 #11

--- src/spec/system/site_layout_spec.rb
+++ src/spec/system/site_layout_spec.rb
@@ -1,20 +1,25 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Homes", type: :system do
   # shared_examples
-  RSpec.shared_examples :go_to_home_page do |id|
-    scenario "homeページに遷移すること" do
+  RSpec.shared_examples "go to home page" do |id|
+    it "homeページに遷移すること" do
       visit root_path
-      click_link("#{id}")
+      click_link(id.to_s)
       expect(page).to have_selector("img[src$='title.png']")
     end
   end
 
-  RSpec.shared_examples :active_class_setting_location do |id|
-    scenario "activeクラスの設定箇所が正しいこと" do
+  RSpec.shared_examples "active class setting location" do |id|
+    it "activeクラスの設定箇所が正しいこと" do
       visit root_path
-      click_link("#{id}")
+      click_link(id.to_s)
       expect(page).to have_selector "##{id}", class: "active"
+    end
+
+    it "activeクラスがページ内で1箇所だけ設定されていること" do
+      visit root_path
+      click_link(id.to_s)
       expect(page.all(".active").size).to eq 1
     end
   end
@@ -22,25 +27,26 @@ RSpec.describe "Homes", type: :system do
   # spec
   describe "header" do
     # [ロゴ画像]をクリックした場合
-    context "click logo" do
-      it_behaves_like :go_to_home_page, :logo
-      it_behaves_like :active_class_setting_location, :home
+    context "when you click logo" do
+      it_behaves_like "go to home page", :logo
+      it_behaves_like "active class setting location", :home
     end
 
     # [ホーム]をクリックした場合
-    context "click home" do
-      it_behaves_like :go_to_home_page, :home
-      it_behaves_like :active_class_setting_location, :home
+    context "when you click home" do
+      it_behaves_like "go to home page", :home
+      it_behaves_like "active class setting location", :home
     end
 
     # [お問い合わせ]をクリックした場合
-    context "click contact" do
-      scenario "contactページに遷移すること" do
+    context "when you click contact" do
+      it "contactページに遷移すること" do
         visit root_path
         click_link("contact")
         expect(page).to have_content("StaticPages#contact")
       end
-      it_behaves_like :active_class_setting_location, :contact
+
+      it_behaves_like "active class setting location", :contact
     end
   end
 end
