80f44a78b0cea14e67a1c5a69b9fc457829b89f1
Hyzsa <ymgshrk.126@gmail.com>
2022-01-25 22:18:55 +0900

    検索時のリクエストスペックを追加 #21

--- src/app/controllers/search_results_controller.rb
+++ src/app/controllers/search_results_controller.rb
@@ -1,22 +1,12 @@
 class SearchResultsController < ApplicationController
+  include SearchResultsHelper
+
   def new
     @shop_info = params[:shop_info]
   end
 
   def create
-    @shop_info = {
-      name: "ここに店名が表示されます。",
-      address: "ここに店舗の住所が表示されます。",
-      mobile_access: "ここに店舗へのアクセス方法が表示されます。",
-      genre: "ジャンル", # genre - name
-      budget: "XXXX円～XXXX円", # budget - average
-      urls: "#",
-      photo: "", # photo - pc - l
-      open: "ここに営業時間が表示されます。",
-      close: "ここに定休日が表示されます。"
-    }
-
+    @shop_info = shop_information
     redirect_to new_search_result_path(shop_info: @shop_info)
-    # render "new" # params確認用
   end
 end
--- src/spec/requests/search_results_spec.rb
+++ src/spec/requests/search_results_spec.rb
@@ -1,29 +1,22 @@
 require "rails_helper"
 
 RSpec.describe "Search Results", type: :request do
-  # describe "GET /search_results/new" do
-  #   example "検索結果画面のHTTPリクエストが成功すること" do
-  #     get new_search_result_path
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-
   describe "POST /search_results" do
-    example "[検索]ボタン押下時のHTTPリクエストが成功すること" do
-      @shop_info = {
-        name: "ここに店名が表示されます。",
-        address: "ここに店舗の住所が表示されます。",
-        mobile_access: "ここに店舗へのアクセス方法が表示されます。",
-        genre: "ジャンル", # genre - name
-        budget: "XXXX円～XXXX円", # budget - average
-        urls: "#",
-        photo: "", # photo - pc - l
-        open: "ここに営業時間が表示されます。",
-        close: "ここに定休日が表示されます。"
-      }
+    shop_info = {
+      name: "ここに店名が表示されます。",
+      address: "ここに店舗の住所が表示されます。",
+      mobile_access: "ここに店舗へのアクセス方法が表示されます。",
+      genre: "ジャンル",
+      budget: "XXXX円～XXXX円",
+      urls: "#",
+      photo: "",
+      open: "ここに営業時間が表示されます。",
+      close: "ここに定休日が表示されます。"
+    }
 
+    example "/search_results/new へのリダイレクトが成功すること" do
       post search_results_path, params: { selected: { range: "", genre: "", budget: "" } }
-      expect(response).to redirect_to(new_search_result_path(shop_info: @shop_info))
+      expect(response).to redirect_to(new_search_result_path(shop_info: shop_info))
     end
   end
 end
