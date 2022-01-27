53006ed7bc0a1d052a4915a2a177bac740e77428
Hyzsa <ymgshrk.126@gmail.com>
2022-01-27 20:03:52 +0900

    スマホのアドレスバー・ツールバーを除いた100vhの値を指定するように修正 #25

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap";
 import "../stylesheets/application.scss";
+import "./common"
 
 Rails.start()
 Turbolinks.start()
