f2faed060c968f4087cbde80b563f968d596ee5e
Hyzsa <ymgshrk.126@gmail.com>
2022-01-30 22:14:37 +0900

    deviseの導入 #29

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  devise_for :users
   get '/contact', to: 'static_pages#contact'
   resources :search_results, only: [:new, :create]
 end
