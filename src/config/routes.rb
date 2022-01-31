0e7ddb220fa98a1db4e500fbd7aa34716a395f5f
Hyzsa <ymgshrk.126@gmail.com>
2022-01-31 17:22:28 +0900

    新規登録時のバリデーションエラー表示処理を修正 #29

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  devise_for :users
   get '/contact', to: 'static_pages#contact'
   resources :search_results, only: [:new, :create]
+  devise_for :users
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+  end
 end
