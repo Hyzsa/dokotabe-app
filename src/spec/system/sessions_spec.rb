c7d72ad021d089bd0f195740e5f403e1304b9d2d
Hyzsa <ymgshrk.126@gmail.com>
2022-02-02 15:04:56 +0900

    ログインのテストを修正 #36

--- src/spec/system/sessions_spec.rb
+++ src/spec/system/sessions_spec.rb
@@ -1,14 +1,15 @@
 require "rails_helper"
 
 RSpec.describe "Sessions", type: :system do
-  let(:user) { create(:user) }
+  let(:valid_user) { create(:user) }
+  let(:invalid_user) { create(:user, confirmed_at: nil) }
 
   before do
     visit new_user_session_path
   end
 
   describe "ログイン" do
-    context "誤った情報でログインした場合" do
+    context "誤ったログイン情報でログインした場合" do
       before do
         fill_in "メールアドレス", with: ""
         fill_in "パスワード", with: ""
@@ -18,20 +19,37 @@ RSpec.describe "Sessions", type: :system do
       example "ログイン画面から遷移しないこと" do
         expect(page).to have_current_path new_user_session_path, ignore_query: true
       end
-
       example "ログイン失敗時のflashメッセージが表示されること" do
         expect(page).to have_selector(".alert-warning", text: "メールアドレスまたはパスワードが違います。")
       end
-
       example "リロードでflashメッセージが消えること" do
         visit new_user_session_path
         expect(page).to_not have_selector(".alert-warning", text: "メールアドレスまたはパスワードが違います。")
       end
     end
 
-    context "正しい情報でログインした場合" do
+    context "本人確認未実施のログイン情報でログインした場合" do
       before do
-        fill_in "メールアドレス", with: user.email
+        fill_in "メールアドレス", with: invalid_user.email
+        fill_in "パスワード", with: "password"
+        click_button "ログイン"
+      end
+
+      example "ログイン画面から遷移しないこと" do
+        expect(page).to have_current_path new_user_session_path, ignore_query: true
+      end
+      example "本人確認未実施のflashメッセージが表示されること" do
+        expect(page).to have_selector(".alert-warning", text: "メールアドレスの本人確認が必要です。")
+      end
+      example "リロードでflashメッセージが消えること" do
+        visit root_path
+        expect(page).to_not have_selector(".alert-warning", text: "メールアドレスの本人確認が必要です。")
+      end
+    end
+
+    context "本人確認実施済みのログイン情報でログインした場合" do
+      before do
+        fill_in "メールアドレス", with: valid_user.email
         fill_in "パスワード", with: "password"
         click_button "ログイン"
       end
@@ -39,11 +57,9 @@ RSpec.describe "Sessions", type: :system do
       example "ホーム画面に遷移すること" do
         expect(page).to have_current_path root_path, ignore_query: true
       end
-
       example "ログイン成功時のflashメッセージが表示されること" do
         expect(page).to have_selector(".alert-success", text: "ログインしました。")
       end
-
       example "リロードでflashメッセージが消えること" do
         visit root_path
         expect(page).to_not have_selector(".alert-success", text: "ログインしました。")
@@ -53,7 +69,7 @@ RSpec.describe "Sessions", type: :system do
 
   describe "ログアウト" do
     before do
-      fill_in "メールアドレス", with: user.email
+      fill_in "メールアドレス", with: valid_user.email
       fill_in "パスワード", with: "password"
       click_button "ログイン"
       click_link "ログアウト"
@@ -62,11 +78,9 @@ RSpec.describe "Sessions", type: :system do
     example "ホーム画面に遷移すること" do
       expect(page).to have_current_path root_path, ignore_query: true
     end
-
     example "ログアウト時のflashメッセージが表示されること" do
       expect(page).to have_selector(".alert-success", text: "ログアウトしました。")
     end
-
     example "リロードでflashメッセージが消えること" do
       visit root_path
       expect(page).to_not have_selector(".alert-success", text: "ログアウトしました。")
