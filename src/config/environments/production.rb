1eeb23765981fc00a99d8698576bd571a183580e
Hyzsa <ymgshrk.126@gmail.com>
2022-02-02 11:54:18 +0900

    mailgunの設定を修正 #36

--- src/config/environments/production.rb
+++ src/config/environments/production.rb
@@ -69,12 +69,14 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   ActionMailer::Base.delivery_method = :smtp
+  host = 'dokotabe-app.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'dokotabe-app.herokuapp.com',
+    :domain         => host,
     :authentication => :plain,
   }
 
