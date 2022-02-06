9ad50029a9a7c1db0ffcd0891116cad1e43d9bf4
Hyzsa <ymgshrk.126@gmail.com>
2022-02-06 15:56:59 +0900

    ログインしている本人のみアクセスできるように設定 #54

--- src/app/controllers/search_histories_controller.rb
+++ src/app/controllers/search_histories_controller.rb
@@ -1,6 +1,15 @@
 class SearchHistoriesController < ApplicationController
   before_action :authenticate_user!, only: [:show]
+  before_action :check_if_login_user, only: [:show]
 
   def show
+  end
+
+  private
+
+  # ログイン中のユーザーか確認
+  def check_if_login_user
+    user = User.find(params[:id])
+    redirect_to root_url unless user == current_user
   end
 end
