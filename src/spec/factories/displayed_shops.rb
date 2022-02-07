cae6af083c449adaaa3d19858dcb020f33b0040b
Hyzsa <ymgshrk.126@gmail.com>
2022-02-07 23:35:56 +0900

    shop_idは一旦使用しないので削除 #54

--- src/spec/factories/displayed_shops.rb
+++ src/spec/factories/displayed_shops.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :displayed_shop do
     user_id { 1 }
-    sequence(:shop_id) { |n| n.to_s }
     shop_name { "XXXXXXXXXXX店" }
     shop_photo do
       "https://images.unsplash.com/photo-1559339352-11d035aa65de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
