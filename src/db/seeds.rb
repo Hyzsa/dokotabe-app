8caf33c136f9f0504a0d56c76b6d4269cb1afdf3
Hyzsa <ymgshrk.126@gmail.com>
2022-02-07 16:49:43 +0900

    日本時間を表示するように修正 #54

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -10,6 +10,6 @@ User.create(
     shop_id: "J001283890",
     shop_name: "尺山寸水 SEKIZAN 有楽町店",
     shop_photo: "https://imgfp.hotp.jp/IMGH/50/61/P033835061/P033835061_168.jpg",
-    displayed_date: Time.now.next_day(n)
+    displayed_date: Time.current.next_day(n)
   )
 end
