d742aa4eba0dc6a4877abdfafa934d1170920c56
Hyzsa <ymgshrk.126@gmail.com>
2022-02-07 18:40:44 +0900

    Userが削除されたときに検索履歴を削除するように設定 #54

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  has_many :displayed_shops
+  has_many :displayed_shops, dependent: :destroy
 end
