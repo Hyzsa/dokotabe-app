a891e8b6896d3073b6766b1e6aacf6dce0515430
Hyzsa <ymgshrk.126@gmail.com>
2022-02-07 19:19:06 +0900

    検索履歴が少ない時にレイアウト崩れが発生するため修正 #54

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -1,16 +1,20 @@
-User.create(
-  email: "user1@example.com",
-  password: "password",
-  confirmed_at: Time.now
-)
-
-30.times do |n|
-  DisplayedShop.create(
-    user_id: 1,
-    shop_id: "J001283890",
-    shop_name: "尺山寸水 SEKIZAN 有楽町店",
-    shop_photo: "https://imgfp.hotp.jp/IMGH/50/61/P033835061/P033835061_168.jpg",
-    shop_url: "https://www.hotpepper.jp/strJ001283890/?vos=nhppalsa000016",
-    displayed_date: Time.current.next_day(n)
+25.times do |n|
+  User.create(
+    email: "user#{n+1}@example.com",
+    password: "password",
+    confirmed_at: Time.current
   )
+end
+
+User.all.each_with_index do |user, i|
+  (i+1).times do |n|
+    user.displayed_shops.create(
+      user_id: 1,
+      shop_id: "#{n}",
+      shop_name: "XXXXXXXXXXX店",
+      shop_photo: "https://images.unsplash.com/photo-1559339352-11d035aa65de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+      shop_url: "https://unsplash.com/photos/YYZU0Lo1uXE",
+      displayed_date: Time.current.next_day(n)
+    )
+  end
 end
