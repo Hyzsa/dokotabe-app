b9d2d280373660238e73847418b441c901e7514d
Hyzsa <ymgshrk.126@gmail.com>
2022-02-07 17:29:04 +0900

    検索履歴から店舗ページに遷移できるようにリンク追加 #54

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -10,6 +10,7 @@ User.create(
     shop_id: "J001283890",
     shop_name: "尺山寸水 SEKIZAN 有楽町店",
     shop_photo: "https://imgfp.hotp.jp/IMGH/50/61/P033835061/P033835061_168.jpg",
+    shop_url: "https://www.hotpepper.jp/strJ001283890/?vos=nhppalsa000016",
     displayed_date: Time.current.next_day(n)
   )
 end
