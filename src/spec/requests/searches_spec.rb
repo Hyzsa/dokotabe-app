f9fa1f4837050105c36c158830a6c35f9df42320
Hyzsa <ymgshrk.126@gmail.com>
2022-02-09 16:09:39 +0900

    rubocopの指摘修正 #61

--- src/spec/requests/searches_spec.rb
+++ src/spec/requests/searches_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe "Searches", type: :request do
       example "ホーム画面にリダイレクトすること" do
         # 必ず店舗が見つからないように、緯度経度にヌル島(0度)を指定
         post search_path, params: { selected: { range: "3",
-                                                        genre: "",
-                                                        budget: "",
-                                                        latitude: "0",
-                                                        longitude: "0" } }
+                                                genre: "",
+                                                budget: "",
+                                                latitude: "0",
+                                                longitude: "0" } }
         expect(response).to redirect_to(root_url)
       end
     end
@@ -18,10 +18,10 @@ RSpec.describe "Searches", type: :request do
       example "検索結果表示画面にリダイレクトすること" do
         # 必ず店舗が見つかるように、緯度経度に東京駅を指定
         post search_path, params: { selected: { range: "3",
-                                                        genre: "",
-                                                        budget: "",
-                                                        latitude: "35.6809591",
-                                                        longitude: "139.7673068" } }
+                                                genre: "",
+                                                budget: "",
+                                                latitude: "35.6809591",
+                                                longitude: "139.7673068" } }
         extracted_shop_info = controller.instance_variable_get(:@extracted_shop_info)
         expect(response).to redirect_to(search_result_url(shop_info: extracted_shop_info))
       end
