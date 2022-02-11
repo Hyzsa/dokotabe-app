d5402b1a791890a7cb47b6d45d782a3bbd8df0b1
Hyzsa <ymgshrk.126@gmail.com>
2022-02-11 15:17:37 +0900

    パスワード再設定画面のシステムスペックをリファクタリング #66

--- src/spec/system/devise/reset_password_spec.rb
+++ src/spec/system/devise/reset_password_spec.rb
@@ -1,108 +1,130 @@
 require "rails_helper"
 
 RSpec.describe "Reset Password", type: :system do
-  before do
-    ActionMailer::Base.deliveries.clear
-    visit root_path
-  end
+  describe "レイアウト確認" do
+    example "パスワード再設定メール送信画面の要素が正しく表示されること" do
+      visit new_user_password_path
+      expect(page).to have_current_path new_user_password_path
 
-  def extract_url(mail)
-    body = mail.body.encoded
-    body[/http[^"]+/]
-  end
-
-  context "パスワード再設定トークンが正しい場合" do
-    example "パスワードを変更できること" do
-      user = create(:user)
-
-      click_link "ログイン"
-
-      # パスワード変更メールを送信する
-      click_link "パスワードを忘れた方はこちら"
-      fill_in "メールアドレス", with: user.email
-      expect { click_button "再設定用メールを送信する" }.to change { ActionMailer::Base.deliveries.size }.by(1)
-      expect(page).to have_content "パスワードの再設定について数分以内にメールでご連絡いたします。"
-
-      # パスワードを変更する
-      mail = ActionMailer::Base.deliveries.last
-      url = extract_url(mail)
-      visit url
-      expect(page).to have_current_path edit_user_password_path, ignore_query: true
-
-      new_password = "123456"
-      fill_in "新しいパスワード", with: new_password
-      fill_in "新しいパスワード（確認用）", with: new_password
-      click_button "パスワードを変更"
-      expect(page).to have_content "パスワードが正しく変更されました。"
-      expect(page).to have_current_path root_path, ignore_query: true
-
-      # ログイン済みになっているため、ログアウトする
-      click_link "ログアウト"
-      expect(page).to have_content "ログアウトしました。"
-
-      # 変更前のパスワードでログインに失敗する
-      click_link "ログイン"
-      fill_in "メールアドレス", with: user.email
-      fill_in "パスワード", with: user.password
-      click_button "ログイン"
-      expect(page).to have_content "メールアドレスまたはパスワードが違います。"
-
-      # 変更後のパスワードでログインに成功する
-      fill_in "メールアドレス", with: user.email
-      fill_in "パスワード", with: new_password
-      click_button "ログイン"
-      expect(page).to have_content "ログインしました。"
+      expect(page).to have_selector "h1", text: "パスワード再設定"
+      expect(page).to have_selector "label[for=user_email]", text: "メールアドレス"
+      expect(page).to have_selector "input#user_email"
+      expect(page).to have_button "再設定用メールを送信する"
     end
-  end
 
-  context "パスワード再設定トークンが間違っている場合" do
-    example "パスワードを変更できないこと" do
-      user = create(:user)
-
-      click_link "ログイン"
-
-      # パスワード変更メールを送信する
-      click_link "パスワードを忘れた方はこちら"
-      fill_in "メールアドレス", with: user.email
-      expect { click_button "再設定用メールを送信する" }.to change { ActionMailer::Base.deliveries.size }.by(1)
-      expect(page).to have_content "パスワードの再設定について数分以内にメールでご連絡いたします。"
-
+    example "パスワード再設定画面の要素が正しく表示されること" do
       # 適当なトークンをクエリで渡してパスワード変更画面にアクセスする
       visit edit_user_password_url(reset_password_token: Devise.friendly_token(10))
       expect(page).to have_current_path edit_user_password_path, ignore_query: true
 
-      # パスワードを変更する
-      new_password = "123456"
-      fill_in "新しいパスワード", with: new_password
-      fill_in "新しいパスワード（確認用）", with: new_password
-      click_button "パスワードを変更"
-      expect(page).to have_content "パスワードリセット用トークンは不正な値です"
-
-      # 変更後のパスワードでログインに失敗する
-      click_link "ログイン"
-      fill_in "メールアドレス", with: user.email
-      fill_in "パスワード", with: new_password
-      click_button "ログイン"
-      expect(page).to have_content "メールアドレスまたはパスワードが違います。"
+      expect(page).to have_selector "h1", text: "パスワード再設定"
+      expect(page).to have_selector "label[for=user_password]", text: "新しいパスワード"
+      expect(page).to have_selector "input#user_password"
+      expect(page).to have_selector "label[for=user_password_confirmation]", text: "新しいパスワード（確認用）"
+      expect(page).to have_selector "input#user_password_confirmation"
+      expect(page).to have_button "パスワードを変更"
     end
   end
 
-  context "パスワード再設定トークンがない場合" do
-    example "パスワードを変更できないこと" do
-      user = create(:user)
+  describe "パスワード変更機能確認" do
+    before do
+      ActionMailer::Base.deliveries.clear
+      visit root_path
+    end
 
-      click_link "ログイン"
+    context "パスワード再設定トークンが正しい場合" do
+      example "パスワードが変更できること" do
+        user = create(:user)
 
-      # パスワード変更メールを送信する
-      click_link "パスワードを忘れた方はこちら"
-      fill_in "メールアドレス", with: user.email
-      expect { click_button "再設定用メールを送信する" }.to change { ActionMailer::Base.deliveries.size }.by(1)
-      expect(page).to have_content "パスワードの再設定について数分以内にメールでご連絡いたします。"
+        click_link "ログイン"
 
-      # クエリなしでパスワード変更画面にアクセスする
-      visit edit_user_password_url
-      expect(page).to_not have_current_path edit_user_password_path, ignore_query: true
-      expect(page).to have_content "このページにはアクセスできません。パスワード再設定メールのリンクからアクセスされた場合には、URL をご確認ください。"
+        # パスワード変更メールを送信する
+        click_link "パスワードを忘れた方はこちら"
+        fill_in "メールアドレス", with: user.email
+        expect { click_button "再設定用メールを送信する" }.to change { ActionMailer::Base.deliveries.size }.by(1)
+        expect(page).to have_content "パスワードの再設定について数分以内にメールでご連絡いたします。"
+
+        # パスワードを変更する
+        mail = ActionMailer::Base.deliveries.last
+        url = extract_url(mail)
+        visit url
+        expect(page).to have_current_path edit_user_password_path, ignore_query: true
+
+        new_password = "123456"
+        fill_in "新しいパスワード", with: new_password
+        fill_in "新しいパスワード（確認用）", with: new_password
+        click_button "パスワードを変更"
+        expect(page).to have_content "パスワードが正しく変更されました。"
+        expect(page).to have_current_path root_path
+
+        # ログイン済みになっているため、ログアウトする
+        click_link "ログアウト"
+        expect(page).to have_content "ログアウトしました。"
+
+        # 変更前のパスワードでログインに失敗する
+        click_link "ログイン"
+        fill_in "メールアドレス", with: user.email
+        fill_in "パスワード", with: user.password
+        click_button "ログイン"
+        expect(page).to have_content "メールアドレスまたはパスワードが違います。"
+
+        # 変更後のパスワードでログインに成功する
+        fill_in "メールアドレス", with: user.email
+        fill_in "パスワード", with: new_password
+        click_button "ログイン"
+        expect(page).to have_content "ログインしました。"
+      end
+    end
+
+    context "パスワード再設定トークンが間違っている場合" do
+      example "パスワードが変更できないこと" do
+        user = create(:user)
+
+        click_link "ログイン"
+
+        # パスワード変更メールを送信する
+        click_link "パスワードを忘れた方はこちら"
+        fill_in "メールアドレス", with: user.email
+        expect { click_button "再設定用メールを送信する" }.to change { ActionMailer::Base.deliveries.size }.by(1)
+        expect(page).to have_content "パスワードの再設定について数分以内にメールでご連絡いたします。"
+
+        # 適当なトークンをクエリで渡してパスワード変更画面にアクセスする
+        visit edit_user_password_url(reset_password_token: Devise.friendly_token(10))
+        expect(page).to have_current_path edit_user_password_path, ignore_query: true
+
+        # パスワードを変更する
+        new_password = "123456"
+        fill_in "新しいパスワード", with: new_password
+        fill_in "新しいパスワード（確認用）", with: new_password
+        click_button "パスワードを変更"
+        expect(page).to have_content "パスワードリセット用トークンは不正な値です"
+
+        # 変更後のパスワードでログインに失敗する
+        click_link "ログイン"
+        fill_in "メールアドレス", with: user.email
+        fill_in "パスワード", with: new_password
+        click_button "ログイン"
+        expect(page).to have_content "メールアドレスまたはパスワードが違います。"
+      end
+    end
+
+    context "パスワード再設定トークンがない場合" do
+      example "パスワードが変更できないこと" do
+        user = create(:user)
+
+        click_link "ログイン"
+
+        # パスワード変更メールを送信する
+        click_link "パスワードを忘れた方はこちら"
+        fill_in "メールアドレス", with: user.email
+        expect { click_button "再設定用メールを送信する" }.to change { ActionMailer::Base.deliveries.size }.by(1)
+        expect(page).to have_content "パスワードの再設定について数分以内にメールでご連絡いたします。"
+
+        # クエリなしでパスワード変更画面にアクセスする
+        visit edit_user_password_url
+        expect(page).to_not have_current_path edit_user_password_path
+        expect(page).to have_content "このページにはアクセスできません。パスワード再設定メールのリンクからアクセスされた場合には、URL をご確認ください。"
+      end
     end
   end
 end
