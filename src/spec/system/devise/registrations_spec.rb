54e331f4eae892f416b8e843c448c4c35e07ac14
Hyzsa <ymgshrk.126@gmail.com>
2022-02-11 14:21:26 +0900

    新規登録機能のシステムスペックをリファクタリング #66

--- src/spec/system/devise/registrations_spec.rb
+++ src/spec/system/devise/registrations_spec.rb
@@ -1,161 +1,153 @@
 require "rails_helper"
 
 RSpec.describe "Registrations", type: :system do
-  before do
-    ActionMailer::Base.deliveries.clear
-    visit root_path
+  describe "レイアウト確認" do
+    example "新規登録画面の要素が正しく表示されること" do
+      visit new_user_registration_path
+      expect(page).to have_current_path new_user_registration_path
+
+      expect(page).to have_selector "h1", text: "新規登録"
+      expect(page).to have_selector "label[for=user_email]", text: "メールアドレス"
+      expect(page).to have_selector "input#user_email"
+      expect(page).to have_selector "label[for=user_password]", text: "パスワード"
+      expect(page).to have_selector "input#user_password"
+      expect(page).to have_selector "label[for=user_password_confirmation]", text: "パスワード（確認用）"
+      expect(page).to have_selector "input#user_password_confirmation"
+      expect(page).to have_button "アカウントを作成"
+    end
   end
 
-  def extract_url(mail)
-    body = mail.body.encoded
-    body[/http[^"]+/]
-  end
+  describe "新規登録機能" do
+    before do
+      ActionMailer::Base.deliveries.clear
+      visit root_path
+      click_link "ログイン"
+      click_link "アカウントの新規登録はこちら"
+    end
 
-  def fill_in_signup_form(new_user)
-    fill_in "メールアドレス", with: new_user.email
-    fill_in "パスワード", with: new_user.password
-    fill_in "パスワード（確認用）", with: new_user.password_confirmation
-  end
-
-  def fill_in_login_form(user)
-    fill_in "メールアドレス", with: user.email
-    fill_in "パスワード", with: user.password
-  end
-
-  context "新規登録フォームへの入力値が有効な場合" do
-    context "本人確認を行っていない場合" do
-      example "ログインできないこと" do
-        click_link "ログイン"
-        click_link "アカウントの新規登録はこちら"
-
-        # アカウントを新規登録する
+    context "新規登録フォームへの入力値が有効な場合" do
+      example "新規登録されること" do
         user = build(:user, confirmed_at: nil)
         fill_in_signup_form(user)
         expect { click_button "アカウントを作成" }.to \
           change { ActionMailer::Base.deliveries.size }.by(1).and \
-            change { User.all.size }.by(1)
-        expect(page).to have_current_path root_path, ignore_query: true
-        expect(page).to have_content "本人確認用のメールを送信しました。メール内のリンクからアカウントを有効化させてください。"
+            change { User.count }.by(1)
 
-        # ログインする
-        click_link "ログイン"
-        fill_in_login_form(user)
-        click_button "ログイン"
-        expect(page).to have_content "メールアドレスの本人確認が必要です。"
-        expect(page).to_not have_selector(".dropdown-toggle", text: "アカウント")
+        expect(page).to have_current_path root_path
+        expect(page).to have_content "本人確認用のメールを送信しました。メール内のリンクからアカウントを有効化させてください。"
       end
     end
 
-    context "本人確認を行った場合" do
-      example "ログインできること" do
-        click_link "ログイン"
-        click_link "アカウントの新規登録はこちら"
+    context "新規登録フォームへの入力値が無効な場合" do
+      context "メールアドレスが未入力の場合" do
+        example "新規登録されないこと" do
+          user = build(:user, email: "", confirmed_at: nil)
+          fill_in_signup_form(user)
+          expect { click_button "アカウントを作成" }.to \
+            change { ActionMailer::Base.deliveries.size }.by(0).and \
+              change { User.count }.by(0)
 
-        # アカウントを新規登録する
-        user = build(:user, confirmed_at: nil)
-        fill_in_signup_form(user)
-        expect { click_button "アカウントを作成" }.to \
-          change { ActionMailer::Base.deliveries.size }.by(1).and \
-            change { User.all.size }.by(1)
-        expect(page).to have_current_path root_path, ignore_query: true
-        expect(page).to have_content "本人確認用のメールを送信しました。メール内のリンクからアカウントを有効化させてください。"
+          expect(page).to have_current_path users_path
+          expect(page).to have_content "メールアドレスを入力してください"
+        end
+      end
 
-        # 本人確認する
-        mail = ActionMailer::Base.deliveries.last
-        url = extract_url(mail)
-        visit url
-        expect(page).to have_content "メールアドレスが確認できました。"
-        expect(page).to have_current_path new_user_session_path, ignore_query: true
+      context "パスワードが未入力の場合" do
+        example "新規登録されないこと" do
+          user = build(:user, password: "", confirmed_at: nil)
+          fill_in_signup_form(user)
+          expect { click_button "アカウントを作成" }.to \
+            change { ActionMailer::Base.deliveries.size }.by(0).and \
+              change { User.count }.by(0)
 
-        # ログインする
-        click_link "ログイン"
-        fill_in_login_form(user)
-        click_button "ログイン"
-        expect(page).to have_content "ログインしました。"
-        expect(page).to have_selector(".dropdown-toggle", text: "アカウント")
+          expect(page).to have_current_path users_path
+          expect(page).to have_content "パスワードを入力してください"
+        end
+      end
+
+      context "パスワード確認が未入力の場合" do
+        example "新規登録されないこと" do
+          user = build(:user, password_confirmation: "", confirmed_at: nil)
+          fill_in_signup_form(user)
+          expect { click_button "アカウントを作成" }.to \
+            change { ActionMailer::Base.deliveries.size }.by(0).and \
+              change { User.count }.by(0)
+
+          expect(page).to have_current_path users_path
+          expect(page).to have_content "パスワード（確認用）とパスワードの入力が一致しません"
+        end
+      end
+
+      context "パスワードが不一致の場合" do
+        example "新規登録されないこと" do
+          user = build(:user, password: "password", password_confirmation: "123456", confirmed_at: nil)
+          fill_in_signup_form(user)
+          expect { click_button "アカウントを作成" }.to \
+            change { ActionMailer::Base.deliveries.size }.by(0).and \
+              change { User.count }.by(0)
+
+          expect(page).to have_current_path users_path
+          expect(page).to have_content "パスワード（確認用）とパスワードの入力が一致しません"
+        end
+      end
+
+      context "パスワードが6文字より短い場合" do
+        example "新規登録されないこと" do
+          user = build(:user, password: "pass", password_confirmation: "pass", confirmed_at: nil)
+          fill_in_signup_form(user)
+          expect { click_button "アカウントを作成" }.to \
+            change { ActionMailer::Base.deliveries.size }.by(0).and \
+              change { User.count }.by(0)
+
+          expect(page).to have_current_path users_path
+          expect(page).to have_content "パスワードは6文字以上で入力してください"
+        end
       end
     end
   end
 
-  context "新規登録フォームへの入力値が無効な場合" do
-    context "メールアドレスが未入力の場合" do
-      example "アカウントが登録されないこと" do
-        click_link "ログイン"
-        click_link "アカウントの新規登録はこちら"
-
-        # アカウントを新規登録する
-        user = build(:user, email: "", confirmed_at: nil)
-        fill_in_signup_form(user)
-        expect { click_button "アカウントを作成" }.to \
-          change { ActionMailer::Base.deliveries.size }.by(0).and \
-            change { User.all.size }.by(0)
-        expect(page).to have_current_path users_path, ignore_query: true
-        expect(page).to have_content "メールアドレスを入力してください"
-      end
+  describe "本人確認機能" do
+    before do
+      ActionMailer::Base.deliveries.clear
     end
 
-    context "パスワードが未入力の場合" do
-      example "アカウントが登録されないこと" do
-        click_link "ログイン"
-        click_link "アカウントの新規登録はこちら"
+    example "本人確認機能が正しく機能していること" do
+      visit root_path
+      click_link "ログイン"
+      click_link "アカウントの新規登録はこちら"
 
-        # アカウントを新規登録する
-        user = build(:user, password: "", confirmed_at: nil)
-        fill_in_signup_form(user)
-        expect { click_button "アカウントを作成" }.to \
-          change { ActionMailer::Base.deliveries.size }.by(0).and \
-            change { User.all.size }.by(0)
-        expect(page).to have_current_path users_path, ignore_query: true
-        expect(page).to have_content "パスワードを入力してください"
-      end
-    end
+      # アカウントを新規登録する
+      user = build(:user, confirmed_at: nil)
+      fill_in_signup_form(user)
+      expect { click_button "アカウントを作成" }.to \
+        change { ActionMailer::Base.deliveries.size }.by(1).and \
+          change { User.count }.by(1)
 
-    context "パスワード確認が未入力の場合" do
-      example "アカウントが登録されないこと" do
-        click_link "ログイン"
-        click_link "アカウントの新規登録はこちら"
+      expect(page).to have_current_path root_path
+      expect(page).to have_content "本人確認用のメールを送信しました。メール内のリンクからアカウントを有効化させてください。"
 
-        # アカウントを新規登録する
-        user = build(:user, password_confirmation: "", confirmed_at: nil)
-        fill_in_signup_form(user)
-        expect { click_button "アカウントを作成" }.to \
-          change { ActionMailer::Base.deliveries.size }.by(0).and \
-            change { User.all.size }.by(0)
-        expect(page).to have_current_path users_path, ignore_query: true
-        expect(page).to have_content "パスワード（確認用）とパスワードの入力が一致しません"
-      end
-    end
+      # ログインできないことを確認する
+      click_link "ログイン"
+      fill_in_login_form(user)
+      click_button "ログイン"
 
-    context "パスワードが不一致の場合" do
-      example "アカウントが登録されないこと" do
-        click_link "ログイン"
-        click_link "アカウントの新規登録はこちら"
+      expect(page).to have_content "メールアドレスの本人確認が必要です。"
+      expect(page).to have_no_selector(".dropdown-toggle", text: "アカウント")
 
-        # アカウントを新規登録する
-        user = build(:user, password: "password", password_confirmation: "123456", confirmed_at: nil)
-        fill_in_signup_form(user)
-        expect { click_button "アカウントを作成" }.to \
-          change { ActionMailer::Base.deliveries.size }.by(0).and \
-            change { User.all.size }.by(0)
-        expect(page).to have_current_path users_path, ignore_query: true
-        expect(page).to have_content "パスワード（確認用）とパスワードの入力が一致しません"
-      end
-    end
+      # 本人確認する
+      mail = ActionMailer::Base.deliveries.last
+      url = extract_url(mail)
+      visit url
+      expect(page).to have_content "メールアドレスが確認できました。"
+      expect(page).to have_current_path new_user_session_path, ignore_query: true
 
-    context "パスワードが6文字より短い場合" do
-      example "アカウントが登録されないこと" do
-        click_link "ログイン"
-        click_link "アカウントの新規登録はこちら"
+      # ログインできることを確認する
+      click_link "ログイン"
+      fill_in_login_form(user)
+      click_button "ログイン"
 
-        # アカウントを新規登録する
-        user = build(:user, password: "pass", password_confirmation: "pass", confirmed_at: nil)
-        fill_in_signup_form(user)
-        expect { click_button "アカウントを作成" }.to \
-          change { ActionMailer::Base.deliveries.size }.by(0).and \
-            change { User.all.size }.by(0)
-        expect(page).to have_current_path users_path, ignore_query: true
-        expect(page).to have_content "パスワードは6文字以上で入力してください"
-      end
+      expect(page).to have_content "ログインしました。"
+      expect(page).to have_selector(".dropdown-toggle", text: "アカウント")
     end
   end
 end
