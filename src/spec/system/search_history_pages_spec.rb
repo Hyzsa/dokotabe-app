e8dfce16e5e25247890a376f9e40d98829a9328e
Hyzsa <ymgshrk.126@gmail.com>
2022-02-14 13:56:14 +0900

    お気に入り機能のシステムスペックを追加 #70

--- src/spec/system/search_history_pages_spec.rb
+++ src/spec/system/search_history_pages_spec.rb
@@ -140,4 +140,78 @@ RSpec.describe "Search History", type: :system do
       end
     end
   end
+
+  describe "お気に入り機能", js: true do
+    example "お気に入り機能が正常に動作すること" do
+      user1 = create(:user)
+      u1_history_list_1 = create_list(:search_history, 5, user_id: user1.id, shop_id: "J000000001" )
+      u1_history_list_2 = create_list(:search_history, 5, user_id: user1.id, shop_id: "J000000002" )
+
+      user2 = create(:user)
+      u2_history_list_1 = create_list(:search_history, 5, user_id: user2.id, shop_id: "J000000001" )
+      u2_history_list_2 = create_list(:search_history, 5, user_id: user2.id, shop_id: "J000000002" )
+
+      # ユーザー1でログインする
+      log_in_as(user1)
+
+      click_link "検索履歴"
+      expect(page).to have_current_path search_history_path(user1.id)
+
+      # お気に入り設定用のリンクがあることを確認する
+      5.times do |n|
+        expect(page).to have_link "", href: favorite_path(history_id: u1_history_list_1[n].id)
+        expect(page).to have_link "", href: favorite_path(history_id: u1_history_list_2[n].id)
+      end
+
+      # お気に入り状態を確認する
+      expect(all("a[data-method=post]").size).to eq 10
+      expect(all("a[data-method=delete]").size).to eq 0
+      expect(all(".favorite").size).to eq 0
+
+      # 1番目に表示されている店舗をお気に入りに登録する
+      click_link "", href: favorite_path(history_id: u1_history_list_1[0].id)
+      sleep(0.5)  # ajax処理待ち
+
+      # 同店舗の履歴がお気に入り状態になることを確認する
+      expect(all("a[data-method=post]").size).to eq 5
+      expect(all("a[data-method=delete]").size).to eq 5
+      expect(all(".favorite").size).to eq 5
+
+      # 1番目と同じ店舗のお気に入りを解除する
+      click_link "", href: favorite_path(history_id: u1_history_list_1[1].id)
+      sleep(0.5)  # ajax処理待ち
+
+      # 同店舗の履歴がお気に入り状態から解除されることを確認する
+      expect(all("a[data-method=post]").size).to eq 10
+      expect(all("a[data-method=delete]").size).to eq 0
+      expect(all(".favorite").size).to eq 0
+
+      # 全店舗お気に入り状態にする
+      click_link "", href: favorite_path(history_id: u1_history_list_1[0].id)
+      click_link "", href: favorite_path(history_id: u1_history_list_2[0].id)
+      sleep(0.5)  # ajax処理待ち
+
+      expect(all("a[data-method=post]").size).to eq 0
+      expect(all("a[data-method=delete]").size).to eq 10
+      expect(all(".favorite").size).to eq 10
+
+      # ユーザー2のお気に入り状態に影響していないことを確認する
+      click_link "アカウント"
+      click_link "ログアウト"
+
+      log_in_as(user2)
+
+      click_link "検索履歴"
+      expect(page).to have_current_path search_history_path(user2.id)
+
+      5.times do |n|
+        expect(page).to have_link "", href: favorite_path(history_id: u2_history_list_1[n].id)
+        expect(page).to have_link "", href: favorite_path(history_id: u2_history_list_2[n].id)
+      end
+
+      expect(all("a[data-method=post]").size).to eq 10
+      expect(all("a[data-method=delete]").size).to eq 0
+      expect(all(".favorite").size).to eq 0
+    end
+  end
 end
