b369b59d54a8f47f0821774ee5b1bf437a2e8660
Hyzsa <ymgshrk.126@gmail.com>
2022-02-14 23:52:24 +0900

    お気に入り画面のシステムスペックを追加 #70

--- src/spec/factories/search_histories.rb
+++ src/spec/factories/search_histories.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :search_history do
     user_id { 1 }
+    sequence(:shop_id) { |n| "J#{n}" }
     shop_name { "XXXXXXXXXXX店" }
     shop_photo do
       "https://images.unsplash.com/photo-1559339352-11d035aa65de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
