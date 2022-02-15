56fed2fc38838c5efb3de78a0813f25edd393b95
Hyzsa <ymgshrk.126@gmail.com>
2022-02-15 18:08:43 +0900

    お気に入り画面のリクエストスペックを追加 #70

--- src/spec/requests/favorites_spec.rb
+++ src/spec/requests/favorites_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Favorites", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  describe "GET /favorite" do
+    context "ログインしている場合" do
+      example "お気に入り画面の表示に成功すること" do
+        sign_in user
+        get favorite_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "ログインしていない場合" do
+      example "ログイン画面にリダイレクトすること" do
+        get favorite_path
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(new_user_session_url)
+      end
+    end
   end
 end
