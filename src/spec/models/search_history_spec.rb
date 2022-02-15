00c76e71a3f00e943f0c0b39b6969f3e8740713e
Hyzsa <ymgshrk.126@gmail.com>
2022-02-15 21:03:19 +0900

    SearchHistoryモデルのスペックを修正

--- src/spec/models/search_history_spec.rb
+++ src/spec/models/search_history_spec.rb
@@ -1,8 +1,44 @@
 require "rails_helper"
 
 RSpec.describe SearchHistory, type: :model do
-  example "正しいデータだと登録できること" do
-    histories = create(:user).search_histories.build
-    expect(histories.save).to be_truthy
+  describe "バリデーション" do
+    example "FactoryBotのデータがバリデーションを通ること" do
+      expect(build(:search_history)).to be_valid
+    end
+
+    example "shop_idが空の場合エラーになること" do
+      history = build(:search_history, shop_id: "")
+      expect(history).to be_invalid
+    end
+
+    example "shop_nameが空の場合エラーになること" do
+      history = build(:search_history, shop_name: "")
+      expect(history).to be_invalid
+    end
+
+    example "shop_photoが空の場合エラーになること" do
+      history = build(:search_history, shop_photo: "")
+      expect(history).to be_invalid
+    end
+
+    example "shop_photoがURL以外の場合エラーになること" do
+      history = build(:search_history, shop_photo: "shop_photo")
+      expect(history).to be_invalid
+    end
+
+    example "displayed_dateが空の場合エラーになること" do
+      history = build(:search_history, displayed_date: "")
+      expect(history).to be_invalid
+    end
+
+    example "shop_urlが空の場合エラーになること" do
+      history = build(:search_history, shop_url: "")
+      expect(history).to be_invalid
+    end
+
+    example "shop_urlがURL以外の場合エラーになること" do
+      history = build(:search_history, shop_url: "shop_url")
+      expect(history).to be_invalid
+    end
   end
 end
