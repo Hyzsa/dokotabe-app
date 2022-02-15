2857180082303a688803739469fd8166d790c88f
Hyzsa <ymgshrk.126@gmail.com>
2022-02-15 22:15:52 +0900

    favoriteモデルのカスタムバリデーション名を変更 #70

--- src/app/models/favorite.rb
+++ src/app/models/favorite.rb
@@ -3,14 +3,14 @@ class Favorite < ApplicationRecord
   belongs_to :search_history
 
   validates :shop_id, presence: true
-  validate :same_shop_exist
+  validate :same_shop_cannot_be_a_favorite
 
   private
 
-  # 同店舗の存在チェック
-  def same_shop_exist
+  # 同じお店のお気に入り追加を防止する
+  def same_shop_cannot_be_a_favorite
     if shop_id.present? && Favorite.where(user_id: user_id ,shop_id: shop_id).exists?
-      errors.add(:shop_id, ": 同じ店舗は追加できません。")
+      errors.add(:shop_id, ": 同じ店舗はお気に入りできません。")
     end
   end
 end
