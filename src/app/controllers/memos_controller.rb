11ba273244507d5145db0bb14af2fa2c6ba5273a
Hyzsa <ymgshrk.126@gmail.com>
2022-02-16 19:09:00 +0900

    メモの新規作成処理を追加 #72

--- src/app/controllers/memos_controller.rb
+++ src/app/controllers/memos_controller.rb
@@ -6,4 +6,21 @@ class MemosController < ApplicationController
     @shop_info = @favorite.search_history
     @memos = @favorite.memos
   end
+
+  def create
+    favorite = Favorite.find(params[:favorite_id])
+    memo = favorite.memos.new(memo_params)
+    if memo.save
+      redirect_to favorite_memos_url
+    else
+      # エラーメッセージをflashで表示する。
+      redirect_to favorite_memos_url, flash: { error: memo.errors.full_messages[0] }
+    end
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:content)
+  end
 end
