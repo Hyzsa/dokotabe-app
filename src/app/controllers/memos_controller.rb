b018508b1716079598a800763d4b821f77333f2e
Hyzsa <ymgshrk.126@gmail.com>
2022-02-16 17:44:25 +0900

    メモ一覧画面の作成 #72

--- src/app/controllers/memos_controller.rb
+++ src/app/controllers/memos_controller.rb
@@ -1,4 +1,9 @@
 class MemosController < ApplicationController
   def index
+    @memo = Memo.new
+    @favorite = Favorite.find(params[:favorite_id])
+
+    @shop_info = @favorite.search_history
+    @memos = @favorite.memos
   end
 end
