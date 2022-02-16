dafa545d9531aa3fa6041524aec27d9637574f5b
Hyzsa <ymgshrk.126@gmail.com>
2022-02-16 12:24:32 +0900

    memoモデルを追加 #72

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_13_080213) do
+ActiveRecord::Schema.define(version: 2022_02_16_031700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2022_02_13_080213) do
     t.integer "user_id"
     t.integer "search_history_id"
     t.string "shop_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "memos", force: :cascade do |t|
+    t.integer "favorite_id"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
---
+++ src/spec/factories/memos.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :memo do
+    favorite_id { 1 }
+    content { "MyText" }
+  end
+end
