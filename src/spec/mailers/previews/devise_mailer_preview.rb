51bf73d4624d34ded4102570930c4cc182b0fc44
Hyzsa <ymgshrk.126@gmail.com>
2022-02-19 17:37:21 +0900

    自動送信されるメールの内容を修正 #78

--- src/spec/mailers/previews/devise_mailer_preview.rb
+++ src/spec/mailers/previews/devise_mailer_preview.rb
@@ -4,13 +4,16 @@ class DeviseMailerPreview < ActionMailer::Preview
     Devise::Mailer.confirmation_instructions(User.new, Devise.friendly_token(10))
   end
 
-  # def email_changed
-  #   Devise::Mailer.email_changed(User.new)
-  # end
+  def email_changed
+    user = User.new
+    user.email = "after@example.com"
+    user.unconfirmed_email = "before@example.com"
+    Devise::Mailer.email_changed(user)
+  end
 
-  # def password_change
-  #   Devise::Mailer.password_change(User.new)
-  # end
+  def password_change
+    Devise::Mailer.password_change(User.new)
+  end
 
   def reset_password_instructions
     Devise::Mailer.reset_password_instructions(User.new, Devise.friendly_token(10))
