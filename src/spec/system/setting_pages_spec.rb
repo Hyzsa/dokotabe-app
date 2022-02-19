728dd5a626c447c6a508f35b43059e6711c14826
Hyzsa <ymgshrk.126@gmail.com>
2022-02-19 21:56:39 +0900

    ユーザー情報編集画面のレイアウトスペックを追加 #78

--- src/spec/system/setting_pages_spec.rb
+++ src/spec/system/setting_pages_spec.rb
@@ -12,7 +12,25 @@ RSpec.describe "Setting Pages", type: :system, js: true do
       expect(page).to have_current_path settings_path
 
       expect(page).to have_selector "h1", text: "設定画面"
+      expect(page).to have_link "ユーザー情報編集", href: user_info_edit_path
       expect(page).to have_link "退会", href: unsubscribe_path
+    end
+
+    example "ユーザー情報編集画面の要素が正しく表示されること" do
+      visit settings_path
+      click_link "ユーザー情報編集", href: user_info_edit_path
+      expect(page).to have_current_path settings_path
+
+      expect(page).to have_selector "h2", text: "ユーザー情報編集"
+      expect(page).to have_selector "label[for=user_email]", text: "メールアドレス"
+      expect(page).to have_selector "input#user_email"
+      expect(page).to have_selector "label[for=user_password]", text: "新しいパスワード"
+      expect(page).to have_selector "input#user_password"
+      expect(page).to have_selector "label[for=user_password_confirmation]", text: "新しいパスワード（確認用）"
+      expect(page).to have_selector "input#user_password_confirmation"
+      expect(page).to have_selector "label[for=user_current_password]", text: "現在のパスワード ※必須"
+      expect(page).to have_selector "input#user_current_password"
+      expect(page).to have_button "変更する"
     end
 
     example "退会画面の要素が正しく表示されること" do
@@ -52,6 +70,10 @@ RSpec.describe "Setting Pages", type: :system, js: true do
       click_link "設定"
       expect(page).to have_current_path settings_path
       expect(page).to have_link "退会", href: unsubscribe_path
+
+      # [退会]ボタンを選択する
+      click_link "退会", href: unsubscribe_path
+      expect(page).to have_content "退会手続き"
 
       # 退会確認で[キャンセル]を選択する
       expect do
